a5f543eda0a960ebbcefb4f8d3858ba496888bbd
FlyingFox365 <tristanhallc2@icloud.com>
2026-04-14 15:13:41 +0100

    Removed unnecessary imports

--- app/src/main/java/com/example/ramanigps/MainActivity.kt
+++ app/src/main/java/com/example/ramanigps/MainActivity.kt
@@ -15,17 +15,14 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import com.example.ramanigps.ui.theme.RamaniGPSTheme
 import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.maps.Style

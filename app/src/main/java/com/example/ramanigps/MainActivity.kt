4d4fa68e8646eb2141c609357af5433e312a0782
FlyingFox365 <tristanhallc2@icloud.com>
2026-04-14 16:34:43 +0100

    App now notifies a user when their location changes

--- app/src/main/java/com/example/ramanigps/MainActivity.kt
+++ app/src/main/java/com/example/ramanigps/MainActivity.kt
@@ -6,13 +6,18 @@ import android.location.LocationListener
 import android.os.Bundle
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.os.Build
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -34,17 +39,24 @@ class MainActivity : ComponentActivity(), LocationListener {
 
     lateinit var locationManager: LocationManager
 
-    // You must create the style builder as an attribute of the activity and then
-    // pass it to composables as a parameter. If you create the style builder on
-    // each recompose, the app may crash!
+    lateinit var notificationManager: NotificationManager
+
     var styleBuilder = Style.Builder().fromUri("https://tiles.openfreemap.org/styles/bright")
     
     val viewModel: GPSViewModel by viewModels()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkPermissions()
+        val channel = NotificationChannel(
+            "LOCATION_CHANNEL",
+            "GPS notifications",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
         setContent {
             RamaniGPSTheme {
                 var latLng by remember { mutableStateOf(LatLng(50.9, -1.4))}
@@ -93,20 +105,22 @@ class MainActivity : ComponentActivity(), LocationListener {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun checkPermissions() {
-        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.POST_NOTIFICATIONS)
 
-        if(checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+        if(permissions.all{ checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }) {
             startGps()
         } else {
-            val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-                if (it) {
+            val launcher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { isGranted ->
+                if (isGranted[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
                     startGps()
-                } else {
-                    Toast.makeText(this, "Cannot access GPS as permission denied", Toast.LENGTH_LONG).show()
+                }
+                if(isGranted[Manifest.permission.POST_NOTIFICATIONS] ==true) {
+                    Toast.makeText(this, "Notification permission granted", Toast.LENGTH_LONG).show()
                 }
             }
-            launcher.launch(permission)
+            launcher.launch(permissions)
         }
     }
 
@@ -116,8 +130,16 @@ class MainActivity : ComponentActivity(), LocationListener {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 5f, this)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onLocationChanged(location: Location) {
         viewModel.latLng = LatLng(location.latitude, location.longitude)
+
+        val notification = Notification.Builder(this, "LOCATION_CHANNEL")
+            .setContentTitle("Location changed")
+            .setContentText("Latitude: ${location.latitude}\nLongitude: ${location.longitude}")
+            .setSmallIcon(org.maplibre.android.R.drawable.maplibre_logo_icon)
+            .build()
+        notificationManager.notify(2616164, notification)
     }
 }
 
